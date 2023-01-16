bfa13db6a381cf1bb41c6461fa5eb1fbbab08983
dongjaekim-hail <dongjaekim@dankook.ac.kr>
2023-01-16 18:29:12 +0900

    initia files

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import gymnasium as gym
+from time import sleep
+env = gym.make('MiniGrid-Empty-5x5-v0',render_mode='human')
 
+observation, info = env.reset()
+for _ in range(1000):
+   action = env.action_space.sample()  # User-defined policy function
+   observation, reward, terminated, truncated, info = env.step(action)
+   env.render()
+   sleep(0.2)
+   if terminated or truncated:
+      observation, info = env.reset()
+env.close()
+# env.render()
--- readme.md
+++ readme.md
@@ -2,3 +2,7 @@
 # Intro
 
 gym 환경을 기반으로 만들어진 multiagent simulation environments.
+
+2023.01.16
+| 일단은 minigrid 기반으로 이것저것 수정해서 만들어보기.
+
